c66d202933a08795bdd9d941ab4dccb53454840f
chung <qc160@naver.com>
2021-04-09 23:46:05 +0900

    vertical bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { render } from 'react-dom';
+import VerticalBar from './VerticalBar.jsx';
 
-function App() {
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles}>
+      <VerticalBar/>
     </div>
-  );
+  )
 }
 
 export default App;
